6bb7d38da435a0b6f1ea0db1cbd6e806a327f9d2
Van Binh Nguyen <binh.pdc01@gmail.com>
2022-06-24 00:57:46 +0800

    Update BFS, pass test cases

--- Project 1/Project 1/BFS.py
+++ Project 1/Project 1/BFS.py
@@ -1,30 +1,199 @@
 import sys
 
+OBSTACLE = -1
+ATTACKED = -2
+VISITED = -3
 # Helper functions to aid in your implementation. Can edit/remove
 #############################################################################
 ######## Piece
 #############################################################################
+
 class Piece:
-    pass
+
+    def __init__(self, position):
+        self.position = position
+
+    def get_reachable_position(self, board):
+        pass
+
+
+class King(Piece):
+    
+    def get_reachable_position(self, board):
+        r, c = self.position
+        reachable = [
+            (r-1, c-1), (r-1, c), (r-1, c+1),
+            (r, c-1), (r, c), (r, c+1),
+            (r+1, c-1), (r+1, c), (r+1, c+1)
+        ]
+        res = list()
+        for pos in reachable:
+            pr, pc = pos
+            if (pr >= 0) and (pr < board.rows) and (pc >= 0) and (pc < board.cols) and (board.grid[pr][pc] != OBSTACLE):
+                res.append(pos)
+        return res
+
+
+class Rook(Piece):
+
+    def get_reachable_position(self, board):
+        r, c = self.position
+        res = list()
+        pr, pc = r, c 
+        while (pr >= 0) and (board.grid[pr][pc] != OBSTACLE):
+            res.append((pr, pc))
+            pr -= 1
+        pr = r + 1
+        while (pr < board.rows) and (board.grid[pr][pc] != OBSTACLE):
+            res.append((pr, pc))
+            pr += 1
+        pr, pc = r, c-1
+        while (pc >= 0) and (board.grid[pr][pc] != OBSTACLE):
+            res.append((pr, pc))
+            pc -= 1 
+        pc = c + 1 
+        while (pc < board.cols) and (board.grid[pr][pc] != OBSTACLE):
+            res.append((pr, pc))
+            pc += 1           
+        return res
+
+
+class Bishop(Piece):
+
+    def get_reachable_position(self, board):
+        r, c = self.position
+        res = list()
+        pr, pc = r, c
+        while (pr >= 0) and (pc >= 0) and (board.grid[pr][pc] != OBSTACLE):
+            res.append((pr, pc))
+            pr -= 1
+            pc -= 1
+        pr, pc = r-1, c+1
+        while (pr >= 0) and (pc < board.cols) and (board.grid[pr][pc] != OBSTACLE):
+            res.append((pr, pc))
+            pr -= 1
+            pc += 1
+        pr, pc = r+1, c-1
+        while (pr < board.rows) and (pc >= 0) and (board.grid[pr][pc] != OBSTACLE):
+            res.append((pr, pc))
+            pr += 1
+            pc -= 1
+        pr, pc = r+1, c+1
+        while (pr < board.rows) and (pc < board.cols) and (board.grid[pr][pc] != OBSTACLE):
+            res.append((pr, pc))
+            pr += 1
+            pc += 1
+        return res
+
+
+class Knight(Piece):
+
+    def get_reachable_position(self, board):
+        r, c = self.position
+        res = list()
+        reachable = [
+            (r-2, c-1), (r-2, c+1), 
+            (r-1, c-2), (r-1, c+2),
+            (r, c),
+            (r+1, c-2), (r+1, c+2),           
+            (r+2, c-1), (r+2, c+1)
+        ]    
+        for pos in reachable:
+            pr, pc = pos
+            if (pr >= 0) and (pr < board.rows) and (pc >= 0) and (pc < board.cols) and (board.grid[pr][pc] != OBSTACLE):
+                res.append(pos)
+        return res
+
+
+class Queen(Bishop, Rook):
+
+    def get_reachable_position(self, board):
+        return list(set(Rook.get_reachable_position(self, board) + Bishop.get_reachable_position(self, board)))
+
 
 #############################################################################
 ######## Board
 #############################################################################
 class Board:
-    pass
+    
+    def __init__(self, grid, rows, cols):
+        self.grid = grid
+        self.rows = rows
+        self.cols = cols
+
 
 #############################################################################
 ######## State
 #############################################################################
 class State:
-    pass
+
+    def __init__(self, board, position, enemy):
+        self.board = board 
+        self.position = position
+
+        for p, pos in enemy:
+            if p == 'King':
+                piece = King(pos)
+            elif p == 'Rook':
+                piece = Rook(pos)
+            elif p == 'Bishop':
+                piece = Bishop(pos)
+            elif p == 'Knight':
+                piece = Knight(pos)
+            elif p == 'Queen':
+                piece = Queen(pos)
+
+            for r, c in piece.get_reachable_position(board):
+                board.grid[r][c] = ATTACKED
+    
+
+#############################################################################
+######## Helping Functions
+#############################################################################
+def to_chess_coord(position):
+    (r, c) = position
+    return (chr(c+97), r)
+
 
 #############################################################################
 ######## Implement Search Algorithm
 #############################################################################
 def search(rows, cols, grid, enemy_pieces, own_pieces, goals):
-    pass
+    
+    board = Board(grid=grid, rows=rows, cols=cols)
+    state = State(board=board, position=own_pieces[0][1], enemy=enemy_pieces)
 
+    frontier = [(state.position, [state.position])]
+    optimal_path = list()
+
+    while frontier:
+        vertex, path = frontier.pop(0)
+        for goal in goals:
+            if vertex == goal:
+                optimal_path = path 
+                break
+
+        r, c = vertex
+        neighbors = [
+            (r-1, c-1), (r-1, c), (r-1, c+1),
+            (r, c-1), (r, c), (r, c+1),
+            (r+1, c-1), (r+1, c), (r+1, c+1)
+        ]
+        for pos in neighbors:
+            pr, pc = pos
+            if (pr >= 0) and (pr < state.board.rows) and (pc >= 0) and (pc < state.board.cols) and (state.board.grid[pr][pc] >= 0):
+                frontier.append((pos, path + [pos]))
+                state.board.grid[pr][pc] = VISITED
+    
+    for i, p in enumerate(optimal_path):
+        optimal_path[i] = to_chess_coord(p)
+
+    result = list()
+
+    for i, p in enumerate(optimal_path[1:]):
+        result.append([optimal_path[i], p])
+
+    return result
 
 #############################################################################
 ######## Parser function and helper functions
---
+++ Project 1/Project 1/main.py
@@ -0,0 +1,6 @@
+from BFS import run_BFS
+from DFS import run_DFS
+from UCS import run_UCS
+from AStar import run_AStar
+
+print(run_BFS())
