c72b710e4601a26a717615fccc865c2d504ad358
Van Binh Nguyen <binh.pdc01@gmail.com>
2022-07-09 16:52:12 +0800

    Use dict to store attacked positions, pass 9/10 test cases

--- Project 2/Project 2/CSP.py
+++ Project 2/Project 2/CSP.py
@@ -1,5 +1,5 @@
 import sys
-import copy
+
 
 OBSTACLE = -1
 # Helper functions to aid in your implementation. Can edit/remove
@@ -16,7 +16,7 @@ class Piece:
         return to_chess_coord(self.position)
 
     def get_type(self):
-        return ""
+        pass
 
     def get_reachable_position(self, board):
         pass
@@ -31,12 +31,7 @@ class King(Piece):
             (r, c-1), (r, c), (r, c+1),
             (r+1, c-1), (r+1, c), (r+1, c+1)
         ]
-        res = list()
-        for pos in reachable:
-            pr, pc = pos
-            if (pr >= 0) and (pr < board.rows) and (pc >= 0) and (pc < board.cols) and (board.grid[pr][pc] != OBSTACLE):
-                res.append(pos)
-        return res
+        return {(pr, pc):True for (pr, pc) in reachable if (pr >= 0) and (pr < board.rows) and (pc >= 0) and (pc < board.cols) and (board.grid[pr][pc] != OBSTACLE)}
     
     def get_type(self):
         return "King"
@@ -46,22 +41,22 @@ class Rook(Piece):
 
     def get_reachable_position(self, board):
         r, c = self.position
-        res = list()
+        res = dict()
         pr, pc = r, c 
         while (pr >= 0) and (board.grid[pr][pc] != OBSTACLE):
-            res.append((pr, pc))
+            res[(pr, pc)] = True
             pr -= 1
         pr = r + 1
         while (pr < board.rows) and (board.grid[pr][pc] != OBSTACLE):
-            res.append((pr, pc))
+            res[(pr, pc)] = True
             pr += 1
         pr, pc = r, c-1
         while (pc >= 0) and (board.grid[pr][pc] != OBSTACLE):
-            res.append((pr, pc))
+            res[(pr, pc)] = True
             pc -= 1 
         pc = c + 1 
         while (pc < board.cols) and (board.grid[pr][pc] != OBSTACLE):
-            res.append((pr, pc))
+            res[(pr, pc)] = True
             pc += 1           
         return res
 
@@ -73,25 +68,25 @@ class Bishop(Piece):
 
     def get_reachable_position(self, board):
         r, c = self.position
-        res = list()
+        res = dict()
         pr, pc = r, c
         while (pr >= 0) and (pc >= 0) and (board.grid[pr][pc] != OBSTACLE):
-            res.append((pr, pc))
+            res[(pr, pc)] = True
             pr -= 1
             pc -= 1
         pr, pc = r-1, c+1
         while (pr >= 0) and (pc < board.cols) and (board.grid[pr][pc] != OBSTACLE):
-            res.append((pr, pc))
+            res[(pr, pc)] = True
             pr -= 1
             pc += 1
         pr, pc = r+1, c-1
         while (pr < board.rows) and (pc >= 0) and (board.grid[pr][pc] != OBSTACLE):
-            res.append((pr, pc))
+            res[(pr, pc)] = True
             pr += 1
             pc -= 1
         pr, pc = r+1, c+1
         while (pr < board.rows) and (pc < board.cols) and (board.grid[pr][pc] != OBSTACLE):
-            res.append((pr, pc))
+            res[(pr, pc)] = True
             pr += 1
             pc += 1
         return res
@@ -104,7 +99,6 @@ class Knight(Piece):
 
     def get_reachable_position(self, board):
         r, c = self.position
-        res = list()
         reachable = [
             (r-2, c-1), (r-2, c+1), 
             (r-1, c-2), (r-1, c+2),
@@ -112,11 +106,7 @@ class Knight(Piece):
             (r+1, c-2), (r+1, c+2),           
             (r+2, c-1), (r+2, c+1)
         ]    
-        for pos in reachable:
-            pr, pc = pos
-            if (pr >= 0) and (pr < board.rows) and (pc >= 0) and (pc < board.cols) and (board.grid[pr][pc] != OBSTACLE):
-                res.append(pos)
-        return res
+        return {(pr, pc):True for (pr, pc) in reachable if (pr >= 0) and (pr < board.rows) and (pc >= 0) and (pc < board.cols) and (board.grid[pr][pc] != OBSTACLE)}
 
     def get_type(self):
         return "Knight"
@@ -125,7 +115,7 @@ class Knight(Piece):
 class Queen(Bishop, Rook):
 
     def get_reachable_position(self, board):
-        return list(set(Rook.get_reachable_position(self, board) + Bishop.get_reachable_position(self, board)))
+        return {**Rook.get_reachable_position(self, board), **Bishop.get_reachable_position(self, board)}
 
     def get_type(self):
         return "Queen"
@@ -153,15 +143,15 @@ class State:
         self.total_assignments = total_assignments
 
     def is_valid(self, assignment):
-        for piece in assignment:
-            attacked_positions = piece.get_reachable_position(self.board)
-            for p1 in assignment:
-                if p1 != piece and p1.position in attacked_positions:
-                    return False
+        piece = assignment[-1]
+        attacked_positions = piece.get_reachable_position(self.board)
+        for p1 in assignment[:-1]:
+            if p1.position in attacked_positions:
+                return False
         return True
 
     def is_complete(self, assignment):
-        return self.total_assignments == len(assignment) and self.is_valid(assignment)
+        return self.total_assignments == len(assignment)
     
 
 #############################################################################
@@ -188,7 +178,6 @@ def search(rows, cols, grid, num_pieces):
         "Knight": num_pieces[4]
     }
     state = State(board=board, variables=variables, total_assignments=total_assignments)
-
     return backtrack(state, list())
 
 
@@ -199,82 +188,29 @@ def backtrack(state, assignment):
 
     if state.is_complete(assignment):
         return {p.get_chess_coord(): p.get_type() for p in assignment}
-
-    # for p in assignment:
-    #     print(p.position, p.get_type(), end=" - ")
-    # print()
     
     var = select_unassigned_variable(state, assignment)
-    for value in order_domain_values(state, assignment):
-        if var == 'King':
-            piece = King(value)
-        elif var == 'Rook':
-            piece = Rook(value)
-        elif var == 'Bishop':
-            piece = Bishop(value)
-        elif var == 'Queen':
-            piece = Queen(value)
-        elif var == 'Knight':
-            piece = Knight(value)
+    domain = order_domain_values(state, assignment)
+    for value in domain:
+        piece = Queen(value) if var == 'Queen' else Rook(value) if var == 'Rook' else \
+            Bishop(value) if var == 'Bishop' else Knight(value) if var == 'Knight' else King(value)
         assignment.append(piece)
-        if forward_check_1(state, assignment):
+        if forward_check(state, assignment):
             if assignment_to_record(assignment) not in invalid_records and state.is_valid(assignment):
                 result = backtrack(state, assignment)
                 if result:
                     return result
-            invalid_records[assignment_to_record(assignment)] = True
         invalid_records[assignment_to_record(assignment)] = True
         assignment.pop()
     return None
 
 
 def forward_check(state, assignment):
-    remainder = {type: state.variables[type] for type in state.variables}
-    for piece in assignment:
-        remainder[piece.get_type()] -= 1
+    return (state.total_assignments - len(assignment)) <= len(order_domain_values(state, assignment))
 
-    for type in remainder:
-        is_valid = False
-
-        if remainder[type] > 0:
-            domain = order_domain_values(state, assignment)
-            for pos in domain:
-                if type == 'King':
-                    piece = King(pos)
-                elif type == 'Rook':
-                    piece = Rook(pos)
-                elif type == 'Bishop':
-                    piece = Bishop(pos)
-                elif type == 'Queen':
-                    piece = Queen(pos)
-                elif type == 'Knight':
-                    piece = Knight(pos)
-                attacked = piece.get_reachable_position(state.board)
-                for p in assignment:
-                    if p.position not in attacked:
-                        is_valid = True
-                        break
-            if not is_valid:
-                return False
-    return True
-
-def forward_check_1(state, assignment):
-    remainder = {type: state.variables[type] for type in state.variables}
-    for piece in assignment:
-        remainder[piece.get_type()] -= 1
-    domain = order_domain_values(state, assignment)
-    if sum(remainder.values()) > len(domain):
-        return False
-    return True
 
 def assignment_to_record(assignment):
-    record = {
-        "King": list(),
-        "Queen": list(),
-        "Bishop": list(),
-        "Rook": list(),
-        "Knight": list()
-    }
+    record = {t: list() for t in ["King", "Queen", "Bishop", "Rook", "Knight"]}
     for piece in assignment:
         record[piece.get_type()].append(piece.position)
     for key in record:
@@ -286,40 +222,18 @@ def select_unassigned_variable(state, assignment):
     remainder = {type: state.variables[type] for type in state.variables}
     for piece in assignment:
         remainder[piece.get_type()] -= 1
-    if "Queen" in remainder and remainder["Queen"] > 0:
-        return "Queen"
-    elif "Rook" in remainder and remainder["Rook"] > 0:
-        return "Rook"
-    elif "Bishop" in remainder and remainder['Bishop'] > 0:
-        return "Bishop"
-    elif "Knight" in remainder and remainder['Knight'] > 0:
-        return "Knight"
-    return "King"
+    return "Queen" if remainder["Queen"] > 0 else "Rook" if remainder["Rook"] > 0 else \
+        "Bishop" if remainder['Bishop'] > 0 else "Knight" if remainder['Knight'] > 0 else "King"
         
 
 def order_domain_values(state, assignment):
-    # invalid_positions = list()
-    # domain_values = list()
-    # for piece in assignment:
-    #     invalid_positions += piece.get_reachable_position(state.board)
-    # for i in range(state.board.rows):
-    #     for j in range(state.board.cols):
-    #         if state.board.grid[i][j] != OBSTACLE and (i, j) not in invalid_positions:
-    #             domain_values.append((i, j))
-    # return domain_values
-
-    grid = copy.deepcopy(state.board.grid)
-    domain_values = list()
+    grid = [row[:] for row in state.board.grid]
     for piece in assignment:
-        attacked = piece.get_reachable_position(state.board)
+        attacked = piece.get_reachable_position(state.board).keys()
         for (i,j) in attacked:
             grid[i][j] = OBSTACLE
-    for i in range(state.board.rows):
-        for j in range(state.board.cols):
-            if grid[i][j] != OBSTACLE:
-                domain_values.append((i, j))
-
-    return domain_values
+    return [(i, j) for i in range(state.board.rows) for j in range(state.board.cols) if grid[i][j] != OBSTACLE]
+    
 
 
 #############################################################################
