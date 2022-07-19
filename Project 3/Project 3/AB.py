3c29cea060e28dab5b7eea590a6a9fac88f7873f
Van Binh Nguyen <binh.pdc01@gmail.com>
2022-07-19 21:37:12 +0800

    Use deepcopy to find next state, win all agents

--- Project 3/Project 3/AB.py
+++ Project 3/Project 3/AB.py
@@ -1,4 +1,4 @@
-import sys
+import copy
 
 WHITE = 1
 BLACK = -1
@@ -28,8 +28,8 @@ class Piece:
 
     def __str__(self):
         if self.color == WHITE:
-            return self.get_type()[:2] + "-W"
-        return self.get_type()[:2] + "-B"
+            return self.get_type() + "-W"
+        return self.get_type() + "-B"
 
 
 class King(Piece):
@@ -194,19 +194,38 @@ class State:
         self.turn = turn
     
     def actions(self):
+        count_check = 0
+        count_capture_queen = 0
+        count_capture_rook = 0
+        count_capture_knight_bishop = 0
         res = list()
         for r in range(ROWS):
             for c in range(COLS):
                 if self.board[r][c] is not None and self.board[r][c].color == self.turn:
                     for x in self.board[r][c].get_reachable_position(self.board):
-                        res.append(((r, c), x))
+                        if self.board[x[0]][x[1]] is not None and self.board[x[0]][x[1]].get_type() == "King":
+                            res.insert(0, ((r, c), x))
+                            count_check += 1
+                        elif self.board[x[0]][x[1]] is not None and self.board[x[0]][x[1]].get_type() == "Queen":
+                            res.insert(count_check, ((r, c), x))
+                            count_capture_queen += 1
+                        elif self.board[x[0]][x[1]] is not None and self.board[x[0]][x[1]].get_type() == "Rook":
+                            res.insert(count_check + count_capture_queen, ((r, c), x))
+                            count_capture_rook += 1
+                        elif self.board[x[0]][x[1]] is not None and self.board[x[0]][x[1]].get_type() != "Pawn":
+                            res.insert(count_check + count_capture_queen + count_capture_rook, ((r, c), x))
+                            count_capture_knight_bishop += 1
+                        elif self.board[x[0]][x[1]] is not None:
+                            res.insert(count_check + count_capture_queen + count_capture_rook + count_capture_knight_bishop, ((r, c), x))
+                        else:
+                            res.append(((r, c), x))
         return res
 
     def result(self, action): # action is a tuple (old_position, new_position)
         board = [row[:] for row in self.board]
         res = State(board, -self.turn)
         res.board[action[0][0]][action[0][1]] = None
-        piece = self.board[action[0][0]][action[0][1]]
+        piece = copy.deepcopy(self.board[action[0][0]][action[0][1]])
         piece.position = action[1]
         res.board[action[1][0]][action[1][1]] = piece
         return res
@@ -217,7 +236,7 @@ class State:
             for c in range(COLS):
                 if self.board[r][c] is not None:
                     if self.board[r][c].get_type() == "King":
-                        max_util += 1000 * self.board[r][c].color
+                        max_util += 1000000 * self.board[r][c].color
                     elif self.board[r][c].get_type() == "Queen":
                         max_util += 90 * self.board[r][c].color
                     elif self.board[r][c].get_type() == "Rook":
@@ -250,18 +269,28 @@ class State:
             res += "\n"
         return res
 
+    def __hash__(self) -> int:
+        res = str(self.turn) + str([[str(piece) for piece in row] for row in self.board])
+        return hash(res)
+
+action_record = dict()
 
 #Implement your minimax with alpha-beta pruning algorithm here.
 def ab(state, depth, alpha, beta, is_max_player):
+    global action_record
+
     if depth == 0 or state.is_terminal():
         return state.utility(), None
     move = None
     if is_max_player:
-        # print("WHITE'S TURN")
         value = -float('inf')
-        actions = state.actions()
+        if hash(state) not in action_record:
+            action_record[hash(state)] = state.actions()
+        actions = action_record[hash(state)]
+        if len(actions) == 0:
+            return 0, None
         for action in actions:
-            v, a = ab(state.result(action), depth-1, alpha, beta, False)
+            v, _ = ab(state.result(action), depth-1, alpha, beta, False)
             if v > value:
                 value, move = v, action
                 alpha = max(alpha, value)
@@ -269,11 +298,14 @@ def ab(state, depth, alpha, beta, is_max_player):
                 break
         return value, move
     else:
-        # print("BLACK'S TURN")
         value = float('inf')
-        actions = state.actions()
+        if hash(state) not in action_record:
+            action_record[hash(state)] = state.actions()
+        actions = action_record[hash(state)]
+        if len(actions) == 0:
+            return 0, None
         for action in actions:
-            v, a = ab(state.result(action), depth-1, alpha, beta, True)
+            v, _ = ab(state.result(action), depth-1, alpha, beta, True)
             if v < value:
                 value, move = v, action
                 beta = min(beta, value)
@@ -288,6 +320,21 @@ def from_chess_coord( ch_coord):
 def to_chess_coord(position):
     (r, c) = position
     return (chr(c+97), r)
+
+def parse_gameboard(gameboard, pieces):
+    for piece in gameboard:
+        if gameboard[piece][0] == 'King':
+            pieces.append(King(from_chess_coord(piece), gameboard[piece][1]))
+        elif gameboard[piece][0] == 'Queen':
+            pieces.append(Queen(from_chess_coord(piece), gameboard[piece][1]))
+        elif gameboard[piece][0] == 'Knight':
+            pieces.append(Knight(from_chess_coord(piece), gameboard[piece][1]))
+        elif gameboard[piece][0] == 'Bishop':
+            pieces.append(Bishop(from_chess_coord(piece), gameboard[piece][1]))
+        elif gameboard[piece][0] == 'Rook':
+            pieces.append(Rook(from_chess_coord(piece), gameboard[piece][1]))
+        else:
+            pieces.append(Pawn(from_chess_coord(piece), gameboard[piece][1]))
 
 ### DO NOT EDIT/REMOVE THE FUNCTION HEADER BELOW###
 # Chess Pieces: King, Queen, Knight, Bishop, Rook (First letter capitalized)
@@ -308,20 +355,7 @@ def studentAgent(gameboard):
     # You can code in here but you cannot remove this function, change its parameter or change the return type
 
     pieces = list()
-
-    for piece in gameboard:
-        if gameboard[piece][0] == 'King':
-            pieces.append(King(from_chess_coord(piece), gameboard[piece][1]))
-        elif gameboard[piece][0] == 'Queen':
-            pieces.append(Queen(from_chess_coord(piece), gameboard[piece][1]))
-        elif gameboard[piece][0] == 'Knight':
-            pieces.append(Knight(from_chess_coord(piece), gameboard[piece][1]))
-        elif gameboard[piece][0] == 'Bishop':
-            pieces.append(Bishop(from_chess_coord(piece), gameboard[piece][1]))
-        elif gameboard[piece][0] == 'Rook':
-            pieces.append(Rook(from_chess_coord(piece), gameboard[piece][1]))
-        else:
-            pieces.append(Pawn(from_chess_coord(piece), gameboard[piece][1]))
+    parse_gameboard(gameboard, pieces)
 
     board = [[None for _ in range(5)] for _ in range(5)]
     for piece in pieces:
@@ -329,7 +363,7 @@ def studentAgent(gameboard):
         board[r][c] = piece
 
     state = State(board)
-
     _, move = ab(state, 4, -float('inf'), float('inf'), True)
     start, end = move
+
     return to_chess_coord(start), to_chess_coord(end)
